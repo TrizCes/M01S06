53a5495d9f46b77bda16fa47a32666eea50993ca
Beatriz Ceschini <biaceschini@gmail.com>
2023-02-10 19:23:09 -0300

    atualiza ex2

--- ex2/index.js
+++ ex2/index.js
@@ -4,11 +4,14 @@ function mostraHorario() {
   const min = horaAtual.getMinutes();
   const sec = horaAtual.getSeconds();
   console.log(`${hora}:${min}:${sec}`);
+
   //return `${hora}:${min}:${sec}`;
+  //OBS: poderia usar o horaAtual.toLocaleDateString('pt-BR'); > já passaria formato HH:MM:SS
 }
 
 const interval = setInterval(() => mostraHorario(), 1000);
 
+//PARA PARAR A REPETIÇÃO:
 setTimeout(() => {
   clearInterval(interval);
 }, 7000);
