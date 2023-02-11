38c5681976fc9b9689be68ea240ce6667ad9cc9c
Beatriz Ceschini <biaceschini@gmail.com>
2023-02-10 23:46:06 -0300

    ex8 começo

---
+++ ex8/index.js
@@ -0,0 +1,42 @@
+const obterUsuarios = async (qtd = 4) => {
+  const url = `https://randomuser.me/api/?results=${qtd}`;
+  try {
+    const response = await fetch(url);
+
+    const data = await response.json();
+
+    return data;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+window.addEventListener('load', async () => {
+  const data = await obterUsuarios();
+
+  const lista = document.getElementById('lista');
+  data.results.forEach((usuario) => {
+    const item = document.createElement('li');
+    const nome = document.createElement('h5');
+    const email = document.createElement('p');
+    const endereco = document.createElement('p');
+    const imagem = document.createElement('img');
+
+    const { street, city, state, country } = usuario.location;
+    const { title, first, last } = usuario.name;
+    const { name, number } = street;
+
+    nome.innerText = `${title} ${first} ${last}`;
+    email.innerText = usuario.email;
+    endereco.innerText = `${name} ${number} ${city} ${state} ${country}`;
+    imagem.src = usuario.picture.large;
+    imagem.alt = 'Imagem do usuario';
+
+    item.appendChild(nome);
+    item.appendChild(email);
+    item.appendChild(endereco);
+    item.appendChild(imagem);
+
+    lista.appendChild(item);
+  });
+});
