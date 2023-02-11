cca2906e81f50502f1221cf909dea7e8ed062389
Beatriz Ceschini <biaceschini@gmail.com>
2023-02-10 22:57:58 -0300

    app idade média

---
+++ ex6/index.js
@@ -0,0 +1,29 @@
+const obterIdadeMedia = async (nome) => {
+  const url = `https://api.agify.io/?country_id=BR&name=${nome}`;
+
+  try {
+    const response = await fetch(url);
+
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const form = document.getElementById('form');
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+  const resultado = document.getElementById('resultado');
+  resultado.innerText = 'Processando...';
+  const nome = event.target.name.value;
+
+  const data = await obterIdadeMedia(nome);
+  if (data) {
+    resultado.innerText = `A idade média é: ${data.age}`;
+    return;
+  }
+
+  resultado.innerText = 'Falha ao buscar informações';
+});
